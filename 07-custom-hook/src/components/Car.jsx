d6f78ffa7ba3bcc2acc524fd0e9f245c848116fa
Jan Smrčka <jan.smrck@gmail.com>
2024-04-26 09:23:10 +0200

    custom hooks

---
+++ 07-custom-hook/src/components/Car.jsx
@@ -0,0 +1,25 @@
+const Car = (props) => {
+  const { brand, model, location, id, image, price, color } = props;
+
+  return (
+    <a href={`/details/${id}`}>
+      <div className="flex w-full rounded-md border p-2 shadow-sm hover:bg-slate-100">
+        <div>
+          <img
+            src={image}
+            alt={model}
+            className="size-12 flex-none rounded-full bg-gray-50"
+          />
+        </div>
+        <div className="pl-5 text-base">
+          <h1 className="text-xl">
+            {brand} {model}
+          </h1>
+          <h2>{`${color} - ${location} - ${price} CZK`}</h2>
+        </div>
+      </div>
+    </a>
+  );
+};
+
+export default Car;

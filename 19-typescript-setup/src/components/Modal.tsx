60c9019f6050c4c593e095511d74b48b52736847
Jan Smrčka <jan.smrck@gmail.com>
2024-05-10 17:30:43 +0200

    typescript intro

---
+++ 19-typescript-setup/src/components/Modal.tsx
@@ -0,0 +1,39 @@
+import React, {
+  FC,
+  MutableRefObject,
+  PropsWithChildren,
+  useEffect,
+  useRef,
+} from "react";
+import { createPortal } from "react-dom";
+
+const Modal: FC<PropsWithChildren> = ({ children }) => {
+  const elRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+
+  useEffect(() => {
+    const modalRoot = document.getElementById("portal");
+    if (!modalRoot || !elRef.current) {
+      return;
+    }
+    modalRoot.appendChild(elRef.current);
+    return () => {
+      if (elRef.current) {
+        modalRoot.removeChild(elRef.current);
+      }
+    };
+  }, []);
+
+  return createPortal(
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/70">
+      <div className="w-full max-w-sm rounded-lg bg-white p-6 shadow-lg">
+        {children}
+      </div>
+    </div>,
+    elRef.current,
+  );
+};
+
+export default Modal;

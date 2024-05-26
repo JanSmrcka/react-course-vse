0f4979eb05353e33bfef3251efabb60aafc6305e
Jan Smrčka <jan.smrck@gmail.com>
2024-05-26 21:46:54 +0200

    fix some bugs

--- hooks-exercises/05-use-effect/exercise-solution/src/api.ts
+++
@@ -1,8 +0,0 @@
-export async function fetchBio(person: string): Promise<string> {
-  const delay = person === 'Bob' ? 4000 : 200
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('This is ' + person + '’s bio.')
-    }, delay)
-  })
-}
